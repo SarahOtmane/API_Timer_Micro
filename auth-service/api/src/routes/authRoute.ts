a9849228f00895152e7868305d54a777e53b2fc5
SarahOtmane <sarahotmane02@gmail.com>
2024-10-11 12:26:04 +0200

    loginUser : config routes

--- auth-service/api/src/routes/authRoute.ts
+++ auth-service/api/src/routes/authRoute.ts
@@ -4,5 +4,6 @@ import * as userController from '../controllers/authController';
 const router = Router();
 
 router.post('/register', userController.registerAUser);
+router.post('/login', userController.loginUser);
 
 export default router;
